459b8281c1b59b9c8b06664ae96407c0e45b99b8
Yudi Prasetya <yudiprasetya7100@gmail.com>
2021-06-23 22:06:14 +0700

    Step CREATE data user

---
+++ resources/views/guru.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<br>
+<br>
+<div class="container" style="min-height: 500px">
+    <div class="card my-3">
+        <div class="card-body">
+            <h1>Data Guru</h1>
+            <a href="#">Tambah Data Guru</a>
+                <table class="table table-bordered table-striped">
+                    <tr>
+                        <th>NIP</th>
+                        <th>Nama</th>
+                        <th>Tempat, Tanggal Lahir</th>
+                        <th>Aksi</th>
+                    </tr>
+                    {{--  @foreach ($products as $product)  --}}
+                    <tr>
+                        <td>12121212</td>
+                        <td>121212</td>
+                        <td>asass, 0291212</td>
+                        <td>
+                           <input type="button" value="Edit" class="btn btn-warning">
+                           <input type="button" value="Hapus" class="btn btn-danger">
+                        </td>
+                    </tr>
+                </table>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\UserController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Blade - Controller
+Route::get('/beranda', [MenuController::class, 'home']);
+Route::get('/siswa', [MenuController::class, 'data_siswa']);
+Route::get('/guru', [MenuController::class, 'data_guru']);
+
+// Route CRUD
+Route::resource('users', (UserController::class));
+Route::resource('siswa', (SiswaController::class));
